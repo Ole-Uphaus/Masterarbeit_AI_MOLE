bf6a6e6da57fdbd2588ec4318f9b098d9999b9ae
Ole Uphaus <ole.uphaus@gmx.de>
2025-11-01 12:36:59 +0100

    Skript erweitert - mehrere Trainingstrajektorien und Prädiktionen mit Gauß Prozess

--- GP/GP_SISO/GP_SISO_IO_test.m
+++ GP/GP_SISO/GP_SISO_IO_test.m
@@ -20,10 +20,12 @@ T_end = 5;
 t_vec = 0:Ts:T_end;
 
 % Input trajectorys
-u_scale_train = 1;
+u_scale_train1 = 1;
+u_scale_train2 = 2;
 u_scale_test = 1.5;
 
-u_vec_train = u_scale_train*sin(2*pi/T_end.*t_vec');
+u_vec_train1 = u_scale_train1*sin(2*pi/T_end.*t_vec');
+u_vec_train2 = u_scale_train2*sin(2*pi/T_end.*t_vec');
 u_vec_test= u_scale_test*sin(2*pi/T_end.*t_vec');
 
 %% Data generation
@@ -38,8 +40,11 @@ opts = odeset( ...
 x0 = [0;
     0];
 
-[~, x_sim] = ode45(@(t,x) oszillator_linear(t, x, u_vec_train, t_vec), t_vec, x0, opts);
-y_sim_train = x_sim(:, 1);
+[~, x_sim] = ode45(@(t,x) oszillator_linear(t, x, u_vec_train1, t_vec), t_vec, x0, opts);
+y_sim_train1 = x_sim(:, 1);
+
+[~, x_sim] = ode45(@(t,x) oszillator_linear(t, x, u_vec_train2, t_vec), t_vec, x0, opts);
+y_sim_train2 = x_sim(:, 1);
 
 [~, x_sim] = ode45(@(t,x) oszillator_linear(t, x, u_vec_test, t_vec), t_vec, x0, opts);
 y_sim_test = x_sim(:, 1);
@@ -49,17 +54,22 @@ y_sim_test = x_sim(:, 1);
 GP_IO = GP_SISO_IO();
 
 % Train Gaussian Process
-u_cell = {u_vec_train};
-y_cell = {y_sim_train};
+u_cell = {u_vec_train1, u_vec_train2};
+y_cell = {y_sim_train1, y_sim_train2};
 GP_IO.train_GP_model(y_cell, u_cell);
+
+% Predict new Trajectory
+[y_pred_test, y_std_test] = GP_IO.predict_trajectory(u_vec_test);
 
 %% Plot
 figure;
 set(gcf, 'Position', [100 100 1200 500]);
 
 subplot(1,2,1);
-plot(t_vec, y_sim_train, LineWidth=1, DisplayName='y-sim-train'); hold on;
+plot(t_vec, y_sim_train1, LineWidth=1, DisplayName='y-sim-train1'); hold on;
+plot(t_vec, y_sim_train2, LineWidth=1, DisplayName='y-sim-train2');
 plot(t_vec, y_sim_test, LineWidth=1, DisplayName='y-sim-test');
+plot(t_vec, y_pred_test, LineWidth=1, DisplayName='y-pred-test');
 grid on;
 xlabel('Zeit [s]'); 
 ylabel('x [m]');
@@ -67,7 +77,8 @@ title('Simulated and Predicted Results');
 legend()
 
 subplot(1,2,2);
-plot(t_vec, u_vec_train, LineWidth=1, DisplayName='u-train'); hold on;
+plot(t_vec, u_vec_train1, LineWidth=1, DisplayName='u-train1'); hold on;
+plot(t_vec, u_vec_train2, LineWidth=1, DisplayName='u-train2');
 plot(t_vec, u_vec_test, LineWidth=1, DisplayName='u-test');
 grid on;
 xlabel('Zeit [s]'); 
