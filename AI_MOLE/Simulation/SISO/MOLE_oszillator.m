d7af3b88dadfb74365dac569853bdb795bc5659b
Ole Uphaus <ole.uphaus@gmx.de>
2025-11-06 15:26:44 +0100

    Weiter an AI-MOLE gearbeitet (noch nicht fertiggestellt)

--- AI_MOLE/Simulation/SISO/MOLE_oszillator.m
+++ AI_MOLE/Simulation/SISO/MOLE_oszillator.m
@@ -2,8 +2,8 @@
 % Autor:      Ole Uphaus
 % Datum:      06.11.2025
 % Beschreibung:
-% In diesem skript werde ich AI-MOLE für den nichtlinearen Oszillator
-% erproben.
+% In diesem skript werde ich AI-MOLE für den nichtlinearen (bzw. wenn man
+% den nichtlinearen Koeffizienten auf null setzt - linearen) Oszillator.
 % -------------------------------------------------------------
 
 clc
@@ -30,23 +30,51 @@ N_iter = 10;
 x0 = [0;
     0];
 
+% Solver settings
+opts = odeset( ...
+    'RelTol', 1e-6, ...         % Tolerance
+    'AbsTol', [1e-8 1e-8], ...  % Tolerance
+    'MaxStep', Ts/5, ...        % Use smaller step size for better Results
+    'InitialStep', Ts/20);
+
+% Initial input Trajectory (simple sin)
+sigma_I = 0.1;
+u_init = sigma_I*sin(2*pi/T_end.*t_vec');
+
 % Initialisation
-SISO_MOLE = SISO_MOLE_IO(r_vec, m_delay);
+SISO_MOLE = SISO_MOLE_IO(r_vec, m_delay, u_init);
+
+%% Run ILC
+
+% % Update Loop
+% u_sim = u_init;
+% [t_sim, x_sim] = ode45(@(t,x) oszillator_nonlinear(t, x, u_sim, t_vec), t_vec, x0, opts);
+% y_sim = x_sim(:, 1);
+% for i = 1:N_iter
+%     % Update input
+%     P = Lifted_dynamics_nonlinear_SISO(@(x) linear_discrete_system(x, Ts), N, m_delay, x_sim);
+%     u_sim = [ILC_Quadr.Quadr_update(y_sim, P); 0];
+% 
+%     % Simulate the system
+%     [t_sim, x_sim] = ode45(@(t,x) oszillator_nonlinear(t, x, u_sim, t_vec), t_vec, x0, opts);
+%     y_sim = x_sim(:, 1);
+% end
+% y_sim_quadratic = y_sim;
 
 %% Plot Results
 figure;
-set(gcf, 'Position', [100 100 1200 500]);
+set(gcf, 'Position', [100 100 1200 800]);
 
-subplot(1,2,1);   % 1 Zeile, 2 Spalten, erster Plot
+subplot(2,2,1);   % 1 Zeile, 2 Spalten, erster Plot
 plot(t_vec, r_vec, LineWidth=1, DisplayName='desired'); hold on;
-% plot(t_vec, y_sim_quadratic, LineWidth=1, DisplayName='ILC Quadr');
+plot(t_vec, y_sim_init, LineWidth=1, DisplayName='init');
 grid on;
 xlabel('Zeit [s]'); 
 ylabel('x [m]');
 title('Compare desired and simulated Trajectory');
 legend()
 
-subplot(1,2,2);   % 1 Zeile, 2 Spalten, erster Plot
+subplot(2,2,3);   % 1 Zeile, 2 Spalten, erster Plot
 % plot(1:length(ILC_Quadr.RMSE_log), ILC_Quadr.RMSE_log, LineWidth=1, DisplayName='ILC Quadr'); hold on;
 % plot(1:length(ILC_PD.RMSE_log), ILC_PD.RMSE_log, LineWidth=1, DisplayName='ILC PD');
 grid on;
@@ -55,4 +83,31 @@ ylabel('RMSE');
 title('Compare error development');
 legend()
 
+subplot(2,2,4);   % 1 Zeile, 2 Spalten, erster Plot
+plot(t_vec, u_init, LineWidth=1, DisplayName='u init'); hold on;
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('F [N]');
+title('Input Signal');
+legend()
+
 %% Local Functions
+function dx = oszillator_nonlinear(t, x_vec, u_vec, t_vec)
+    % Simulation parameters
+    m  = 2; % kg
+    c1 = 2; % N/m
+    c2 = 2; % N/m^3
+    d  = 0.5; % Ns/m
+
+    % States
+    x = x_vec(1);
+    xp = x_vec(2);
+
+    % Input
+    u = interp1(t_vec, u_vec, t, 'previous', 'extrap');
+
+    % Dynamics
+    dx = zeros(2, 1);
+    dx(1) = xp;
+    dx(2) = 1/m*(-c1*x - c2*x^3 - d*xp + u);
+end
--- AI_MOLE/Simulation/SISO/SISO_MOLE_IO.m
+++ AI_MOLE/Simulation/SISO/SISO_MOLE_IO.m
@@ -3,15 +3,18 @@ classdef SISO_MOLE_IO < handle
     %   Detailed explanation goes here
     
     properties
-        Property1
+        GP_SISO
+        ILC_SISO
+        y_cell
+        u_cell
     end
     
     methods
-        function obj = SISO_MOLE_IO()
+        function obj = SISO_MOLE_IO(r_vec, m_delay, u_init)
             %SISO_MOLE_IO Construct an instance of this class
             %   Detailed explanation goes here
             
-            % Geerate dynamic paths
+            % Generate dynamic paths
             base_dir = fileparts(mfilename("fullpath"));
             GP_path = fullfile(base_dir, '..', '..', '..', 'GP', 'GP_SISO');
             ILC_path = fullfile(base_dir, '..', '..', '..', 'ILC', 'ILC_SISO');
@@ -19,8 +22,14 @@ classdef SISO_MOLE_IO < handle
             % Add temporary paths
             addpath(GP_path);
             addpath(ILC_path);
+
+            % Initialize Classes
+            obj.GP_SISO = GP_SISO_IO();
+            obj.ILC_SISO = ILC_SISO(r_vec, m_delay, u_init);
         end
 
+        function update_input(obj, y_vec)
+        end
     end
 end
 
