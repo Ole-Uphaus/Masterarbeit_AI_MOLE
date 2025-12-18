3606de3af5d40fb4382eda1d8a68e8b5cdae366b
Ole Uphaus <ole.uphaus@gmx.de>
2025-12-18 10:45:23 +0100

    begonnen mit AI-MOLE initialisierung - vorher jedoch Torsionsschwinger simulativ untersuchen

--- AI_MOLE/Test_Bench/Torsion_Oscillator/Init_MOLE.m
+++ AI_MOLE/Test_Bench/Torsion_Oscillator/Init_MOLE.m
@@ -4,3 +4,21 @@
 % Beschreibung:
 % Dieses Skript dient dazu, ein neuen AI-MOLE Run zu initialisieren.
 % -------------------------------------------------------------
+
+clc
+clear
+close all
+
+%% Parameters
+% General ILC Type ('free', 'serial', 'parallel')
+ilc_type = 'free';
+
+% Choose reference trajectory
+traj_name = 'Trajectory_01.mat';
+
+%% Load reference Trajectory
+% File path
+traj_path = fullfile(pwd, 'Reference_Trajectories', traj_name);
+
+% Load Trajectory Data
+load(traj_path);
