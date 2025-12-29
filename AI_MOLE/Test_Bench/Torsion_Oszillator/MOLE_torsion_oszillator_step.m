38468be4aa45c8adbaffd92d070026c5d79661b7
Ole Uphaus <ole.uphaus@gmx.de>
2025-12-29 13:57:50 +0100

    AI-MOLE updateskript für jeweils einen iterationsschritt erstellt.

--- AI_MOLE/Test_Bench/Torsion_Oszillator/MOLE_torsion_oszillator_step.m
+++ AI_MOLE/Test_Bench/Torsion_Oszillator/MOLE_torsion_oszillator_step.m
@@ -7,3 +7,107 @@
 % ausgelesen und anschließend die optimierte Eingangstrajektorie
 % zurückgegeben.
 % -------------------------------------------------------------
+
+clc
+clear
+close all
+
+% Generate Dynamic file Path
+base_dir = fileparts(mfilename("fullpath"));
+MOLE_Path = fullfile(base_dir, '..', '..', 'Simulation', 'SISO');
+addpath(MOLE_Path);
+
+%% Load MOLE and simulation/trial 
+% MOLE object
+date_string = '2025_12_29';
+run_filename = 'Run_01_uncontrolled.mat';
+run_filepath = fullfile(pwd, 'Runs', date_string, run_filename);
+
+% Current Simulation/Trial
+name = erase(run_filename, '.mat');
+parts = split(name, '_');
+sim_trial_filename = sprintf('Sim_Run_%s_%s.mat', parts{2}, date_string);
+
+% Load files
+load(run_filepath);
+load(sim_trial_filename);
+
+%% MOLE Update
+% Check if timestamps are in the right order (this prevents from performing
+% an update before performing a new simulation/trial)
+if init_update_timestamp < sim_trial_timestamp
+    % Chech if this was the last iteration (no update will be performed - just
+    % the last trajectory will be saved)
+    idx = find(~cellfun('isempty', SISO_MOLE.u_cell), 1, 'last');
+
+    if idx <= SISO_MOLE.N_iter
+        % Perform MOLE update
+        [~] = SISO_MOLE.update_input(y_vec);
+        disp('AI-MOLE Update durchgeführt.')
+    elseif idx == SISO_MOLE.N_iter+1
+        % Save last trajectory
+        SISO_MOLE.save_final_trajectory(y_sim);
+        disp('Letzte trajektorie gespeichert.')
+    else
+        disp('Es wurde kein Update durchgeführt, da die maximale Anzahl an Iterationen erreicht wurde.')
+    end
+else
+    disp('Es wurde kein Update durchgeführt, da zuerst ein neuer Trial/Simulation benötigt wird.')
+end
+
+%% Save results
+% Overwrite existing .mat-file
+init_update_timestamp = datetime('now');    % Timestamp of update
+save(run_filepath, 'SISO_MOLE', 'ref_traj', 'init_update_timestamp');
+
+%% Plot results
+figure;
+set(gcf, 'Position', [100 100 1200 800]);
+
+subplot(2,2,1);   % 1 Zeile, 2 Spalten, erster Plot
+plot(ref_traj.t_vec, ref_traj.phi2, LineWidth=1, DisplayName='desired'); hold on;
+for i = 1:SISO_MOLE.N_iter
+    if ~isempty(SISO_MOLE.y_cell{i})
+        plot(ref_traj.t_vec, SISO_MOLE.y_cell{i}, LineWidth=1, Color=[0.5 0.5 0.5], HandleVisibility='off');
+    end
+end
+if ~isempty(SISO_MOLE.y_cell{SISO_MOLE.N_iter+1})
+    plot(ref_traj.t_vec, SISO_MOLE.y_cell{SISO_MOLE.N_iter+1}, LineWidth=1, DisplayName=sprintf('Iteration %d', SISO_MOLE.N_iter));
+end
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('x [m]');
+title('Compare desired and simulated Trajectory');
+legend('Location', 'best');
+
+subplot(2,2,3);   % 1 Zeile, 2 Spalten, erster Plot
+plot(0:(length(SISO_MOLE.ILC_SISO.RMSE_log)-1), SISO_MOLE.ILC_SISO.RMSE_log, LineWidth=1, DisplayName='ILC Quadr');
+grid on;
+xlabel('Iteration'); 
+ylabel('RMSE');
+title('Compare error development');
+legend()
+
+subplot(2,2,4);   % 1 Zeile, 2 Spalten, erster Plot
+hold on;
+for i = 1:SISO_MOLE.N_iter
+    if ~isempty(SISO_MOLE.u_cell{i})
+        plot(ref_traj.t_vec, SISO_MOLE.u_cell{i}, LineWidth=1, Color=[0.5 0.5 0.5], HandleVisibility='off');
+    end
+end
+if ~isempty(SISO_MOLE.u_cell{SISO_MOLE.N_iter+1})
+    plot(ref_traj.t_vec, SISO_MOLE.u_cell{SISO_MOLE.N_iter+1}, LineWidth=1, DisplayName=sprintf('Iteration %d', SISO_MOLE.N_iter));
+end
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('F [N]');
+title('Input Signal');
+legend('Location', 'best');
+
+subplot(2,2,2);
+% plot(t_vec, v_vec, LineWidth=1, DisplayName='meas');
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('x [m]');
+title('Noise');
+legend()
