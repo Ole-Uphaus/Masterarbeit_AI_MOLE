0f6f2bf3d289c0c55e308c6ab9e453ff1644a5f7
Ole Uphaus <ole.uphaus@gmx.de>
2026-01-08 17:17:44 +0100

    Berechnung des Approximierten Systemeingangs durchgeführt

--- AI_MOLE/Test_Bench/Torsion_Oszillator/MOLE_torsion_oszillator_step.m
+++ AI_MOLE/Test_Bench/Torsion_Oszillator/MOLE_torsion_oszillator_step.m
@@ -26,6 +26,11 @@ run_filepath = fullfile(pwd, 'Runs', date_string, run_filename);
 % Current Simulation/Trial
 sim_trial_filename = sprintf('Trial_%s.mat', date_string);
 
+% Extract architecture
+name = erase(run_filename, '.mat');
+parts = split(name, '_');
+architecture = parts{3};
+
 % Load files
 load(run_filepath);
 load(sim_trial_filename);
@@ -34,14 +39,14 @@ load(sim_trial_filename);
 save_results = false;
 
 %% MOLE Update
+idx_u = find(~cellfun('isempty', SISO_MOLE.u_cell), 1, 'last');
+idx_y = find(~cellfun('isempty', SISO_MOLE.y_cell), 1, 'last');
+
 % Check if timestamps are in the right order (this prevents from performing
 % an update before performing a new simulation/trial)
 if init_update_timestamp < sim_trial_timestamp
     % Chech if this was the last iteration (no update will be performed - just
     % the last trajectory will be saved)
-    idx_u = find(~cellfun('isempty', SISO_MOLE.u_cell), 1, 'last');
-    idx_y = find(~cellfun('isempty', SISO_MOLE.y_cell), 1, 'last');
-
     if idx_u <= SISO_MOLE.N_iter
         % Perform MOLE update
         [~] = SISO_MOLE.update_input(y_vec);
@@ -66,6 +71,78 @@ if save_results
     % Overwrite existing .mat-file
     init_update_timestamp = datetime('now');    % Timestamp of update
     save(run_filepath, 'SISO_MOLE', 'ref_traj', 'init_update_timestamp');
+end
+
+%% Calculate approximate actuator input
+switch architecture
+    case 'serial'
+        % Sample Time
+        Ts = 0.001;
+        
+        % Simulation parameters
+        J1  = 0.0299;    % kgm^2
+        J2  = 0.0299;    % kgm^2
+        c_phi = 7.309;   % Nm/rad
+        d_v1 = 0.055;    % Nms/rad
+        d_v2 = 0.0064;   % Nms/rad
+        
+        % State space
+        A = [0, 1, 0, 0;
+            -c_phi/J1, -d_v1/J1, c_phi/J1, 0;
+            0, 0, 0, 1;
+            c_phi/J2, 0, -c_phi/J2, -d_v2/J2];
+        
+        b = [0;
+            1/J1;
+            0;
+            0];
+        
+        c_T = [0, 0, 1, 0];
+        
+        d = 0;
+        
+        % Discrete System
+        sys_contin = ss(A, b, c_T, 0);
+        sys_disc = c2d(sys_contin, Ts, 'zoh');
+        
+        % System matrices
+        Ad = sys_disc.A;
+        bd = sys_disc.B;
+        c_Td = sys_disc.C;
+        dd = sys_disc.D;
+        
+        % LQR weighting matrices (as in DR)
+        Q_LQR = diag([1, 1, 10, 1]);
+        R_LQR = 1;
+        
+        % LQR gain
+        k_T_disc = dlqr(Ad, bd, Q_LQR, R_LQR);
+        
+        % Controlled system dynamics
+        Ad_cont = Ad - bd * k_T_disc;
+        sys_disc_cont = ss(Ad_cont, bd, c_Td, dd, Ts);
+
+        % Upsample input trajectory
+        t_vec_sys = 0:Ts:ref_traj.t_vec(end);
+        t_vec_u = ref_traj.t_vec;
+        
+        u_vec_sys = interp1(t_vec_u, SISO_MOLE.u_cell{idx_u}, t_vec_sys, 'previous', 'extrap');
+
+        % Simulate system
+        x0 = [0; 0; 0; 0];
+        [~, ~, x_sim] = lsim(sys_disc_cont, u_vec_sys(:), t_vec_sys(:), x0);
+
+        % Calculate actuator input
+        u_vec_actuator = u_vec_sys(:) - x_sim*k_T_disc.';
+
+    case 'uncontrolled'
+        % Upsample input trajectory
+        t_vec_sys = 0:Ts:ref_traj.t_vec(end);
+        t_vec_u = ref_traj.t_vec;
+        
+        u_vec_sys = interp1(t_vec_u, SISO_MOLE.u_cell{idx_u}, t_vec_sys, 'previous', 'extrap');
+    otherwise
+        error('Architektur nicht erkannt.')
 end
 
 %% Plot results
@@ -120,3 +197,11 @@ xlabel('Zeit [s]');
 ylabel('x [m]');
 title('Noise');
 legend()
+
+figure;
+plot(t_vec_sys, u_vec_actuator, LineWidth=1);
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('F [N]');
+title('Approximate Actuator Input Signal');
+ylim([-9 9]);
