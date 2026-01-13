26ffd55f5726d7ffeb76ce48c90f088425c39bb1
Ole Uphaus (Pruefstand) <ole.uphaus@gmx.de>
2026-01-13 15:10:42 +0100

    Weiterer Run am Prüfstand

--- AI_MOLE/Test_Bench/Torsion_Oszillator/Init_MOLE.m
+++ AI_MOLE/Test_Bench/Torsion_Oszillator/Init_MOLE.m
@@ -81,11 +81,11 @@ switch architecture
         params.weight_init_method = 'Stochastic';
         
         % Choose nonlinearity damping method ('none', 'relative_1', 'relative_2', 'minimize')
-        params.nonlin_damping = 'minimize';
+        params.nonlin_damping = 'none';
         params.beta = 0;
         
         % Initial input Trajectory (simple sin or static feed forward)
-        use_feedforward_control = false;
+        use_feedforward_control = true;
 
         if use_feedforward_control
             u_init = S_gain .* r_vec;
