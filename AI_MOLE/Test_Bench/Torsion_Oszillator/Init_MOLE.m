4d7bc68398e02256330b42031e80979eebf66629
Ole Uphaus (Pruefstand) <ole.uphaus@gmx.de>
2026-01-14 09:10:21 +0100

    AI-MOLE RUn mit meindls konfiguration

--- AI_MOLE/Test_Bench/Torsion_Oszillator/Init_MOLE.m
+++ AI_MOLE/Test_Bench/Torsion_Oszillator/Init_MOLE.m
@@ -78,7 +78,7 @@ switch architecture
         
         % Choose weight initialisation Method ('Meindl', 'Stochastic', 'Heuristic',
         % 'Robust', 'Manual')
-        params.weight_init_method = 'Stochastic';
+        params.weight_init_method = 'Meindl';
         
         % Choose nonlinearity damping method ('none', 'relative_1', 'relative_2', 'minimize')
         params.nonlin_damping = 'none';
