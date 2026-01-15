f6cdec53867825e2dd10d5a5b8d7a6cfa6b6568d
Ole Uphaus <ole.uphaus@gmx.de>
2026-01-15 08:40:54 +0100

    Plot Skript erweitert

--- ILC/Test_Bench/Torsion_Oszillator/ILC_Plot.m
+++ ILC/Test_Bench/Torsion_Oszillator/ILC_Plot.m
@@ -21,11 +21,14 @@ addpath(GP_Path);
 
 %% Load MOLE object
 % MOLE object
-date_string = '2026_01_10';
-run_filename = 'Run_03_serial.mat';
+date_string = '2026_01_14';
+run_filename = 'Run_01_serial.mat';
 run_filepath = fullfile(pwd, 'Runs', date_string, run_filename);
 
 load(run_filepath);
+
+% Iteration to plot
+plot_iter = 10;
 
 %% Plot results
 % Get number of iterations
@@ -68,6 +71,52 @@ for i = 1:N_iter
 end
 if ~isempty(ILC_Quadr.u_cell{N_iter+1})
     plot(ref_traj.t_vec, ILC_Quadr.u_cell{N_iter+1}, LineWidth=1, DisplayName=sprintf('Iteration %d', N_iter));
+end
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('T [Nm]');
+title('Input Signal');
+legend('Location', 'best');
+
+subplot(2,2,2);
+% plot(t_vec, v_vec, LineWidth=1, DisplayName='meas');
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('phi2 [rad]');
+title('Noise');
+legend()
+
+% Plot 2
+
+figure;
+set(gcf, 'Position', [100 100 1200 800]);
+
+subplot(2,2,1);   % 1 Zeile, 2 Spalten, erster Plot
+plot(ref_traj.t_vec, ref_traj.phi2, LineWidth=1, DisplayName='desired'); hold on;
+if ~isempty(ILC_Quadr.y_cell{plot_iter+1})
+    plot(ref_traj.t_vec, ILC_Quadr.y_cell{plot_iter+1}, LineWidth=1, DisplayName=sprintf('Iteration %d', plot_iter));
+end
+grid on;
+xlabel('Zeit [s]'); 
+ylabel('phi2 [rad]');
+title('Compare desired and simulated Trajectory');
+legend('Location', 'best');
+
+subplot(2,2,3);   % 1 Zeile, 2 Spalten, erster Plot
+% plot(0:(length(ILC_Quadr.RMSE_log)-1), ILC_Quadr.RMSE_log, LineWidth=1, DisplayName='ILC Quadr'); hold on;
+semilogy(0:(length(ILC_Quadr.RMSE_log)-1), ILC_Quadr.RMSE_log, LineWidth=1, DisplayName='ILC Quadr'); hold on;
+trial_vec = 0:(length(ILC_Quadr.RMSE_log)-1);
+plot(trial_vec(plot_iter+1), ILC_Quadr.RMSE_log(plot_iter+1), 'ro', 'MarkerSize', 5, 'MarkerFaceColor', 'r')
+grid on;
+xlabel('Iteration'); 
+ylabel('RMSE');
+title('Compare error development');
+legend()
+
+subplot(2,2,4);   % 1 Zeile, 2 Spalten, erster Plot
+hold on;
+if ~isempty(ILC_Quadr.u_cell{plot_iter+1})
+    plot(ref_traj.t_vec, ILC_Quadr.u_cell{plot_iter+1}, LineWidth=1, DisplayName=sprintf('Iteration %d', plot_iter));
 end
 grid on;
 xlabel('Zeit [s]'); 
