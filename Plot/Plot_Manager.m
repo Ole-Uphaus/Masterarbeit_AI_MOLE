85d5cbba779ca8635936ec3198f927e5cb94820c
Ole Uphaus <ole.uphaus@gmx.de>
2026-02-24 12:28:33 +0100

    plots weitergeführt

--- Plot/Plot_Manager.m
+++ Plot/Plot_Manager.m
@@ -445,6 +445,7 @@ classdef Plot_Manager < handle
                 % Set the y-scale
                 if i == 2
                     opts.y_scale = 'log';
+                    ax.YTick = [1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1 1e2 1e3 1e4];  
                 else
                     opts.y_scale = 'linear';
                 end
