4d942a3a06f67296c26565ce35dffe3f7a8ea748
Ole Uphaus <ole.uphaus@gmx.de>
2026-03-02 16:57:34 +0100

    Zustandsregelung für Pneumatikzylinder entworfen

--- Practice/Control/Pneumatic_Cylinder/V2_Pneumatikzylinder_ini_Template.m
+++ Practice/Control/Pneumatic_Cylinder/V2_Pneumatikzylinder_ini_Template.m
@@ -33,10 +33,10 @@ b_1 = 20; % [kg/m]
 A  = pi*d^2/4;  % Kolbenfläche
 
 % Reglerparameter für kaskadierte Regelung des Druckes und der Position
-gamma_p = 200;  % Regelung Druck           [100 .. 1000]
-gamma_1 = 50;  % Regelung Position        [ 50 ..  500] 
-gamma_2 = 20;  % Regelung Geschwindigkeit [ 20 .. 1000]
-gamma_3 = 500;  % Schätzung Störfehler     [500 .. 3000]
+gamma_p = 500;  % Regelung Druck           [100 .. 1000]
+gamma_1 = 300;  % Regelung Position        [ 50 ..  500] 
+gamma_2 = 200;  % Regelung Geschwindigkeit [ 20 .. 1000]
+gamma_3 = 2000;  % Schätzung Störfehler     [500 .. 3000]
 
 %% Solltrajektorien generieren
 
@@ -69,4 +69,39 @@ a5s = double(solution.a5);
 t_vec = linspace(0, 1, 200);
 y_d_plot = a0s + a1s*t_vec + a2s*t_vec.^2 + a3s*t_vec.^3 + a4s*t_vec.^4 + a5s*t_vec.^5;
 
-%plot(t_vec, y_d_plot)
+plot(t_vec, y_d_plot)
+
+%% Entwurf Optimalregelung (LQR) des mechanischen Teilsystems
+% Sample Time
+Ts = 0.001;
+
+% Zustandsraummodell
+A_contin = [0 1;
+    0 0];
+b_contin = [0;
+    1/m];
+c_T = [1, 0];
+
+% Diskretisierung
+sys_contin = ss(A_contin, b_contin, c_T, 0);
+sys_disc = c2d(sys_contin, Ts, 'zoh');
+
+% Diskretes Zustandsraummodell
+Ad = sys_disc.A;
+bd = sys_disc.B;
+c_Td = sys_disc.C;
+dd = sys_disc.D;
+
+% LQR Gewichtungsmatrizen
+Q_lqr = diag([1e5, 1]);
+r_lqr = 1;
+
+% Reglerverstärkung
+k_T = dlqr(Ad, bd, Q_lqr, r_lqr);
+
+% Dynamik des geregelten Systems
+Ad_cont = Ad - bd * k_T;
+sys_disc_cont = ss(Ad_cont, bd, c_Td, dd, Ts);
+
+% Statische Verstärkung berechnen
+S_gain = 1 / dcgain(sys_disc_cont);
