30e1bfff595a6db4a9146a95fb9a5dab03618b7c
Ole Uphaus <ole.uphaus@gmx.de>
2026-03-11 15:17:38 +0100

    plot opdate

--- AI_MOLE/Simulation/SISO/Thesis_plots/Oszillator/Plot_compare_beta_heuristic.m
+++ AI_MOLE/Simulation/SISO/Thesis_plots/Oszillator/Plot_compare_beta_heuristic.m
@@ -42,27 +42,29 @@ iter_vec = 0:data_heuristic.SISO_MOLE.N_iter;
 % Assign values (args)
 args = struct();
 
-args.x_cell = {iter_vec, iter_vec};
-args.y_cell = {{data_heuristic.SISO_MOLE.ILC_SISO.RMSE_log, data_stochastic.SISO_MOLE.ILC_SISO.RMSE_log, data_modelbased.ILC_Quadr.RMSE_log}, 
-    {data_varbeta.SISO_MOLE_Cell{1, 1}.ILC_SISO.RMSE_log, data_stochastic.SISO_MOLE.ILC_SISO.RMSE_log, data_varbeta.SISO_MOLE_Cell{2, 1}.ILC_SISO.RMSE_log, data_varbeta.SISO_MOLE_Cell{3, 1}.ILC_SISO.RMSE_log}};
-args.x_label_cell = {'Iteration', 'Iteration'};
-args.y_label_cell = {'RMSE in $\mathrm{m}$', ''};
-args.title_cell = {'\textbf{(a)}', '\textbf{(b)}'};
-args.legend_cell = {{'MOLE', 'MOLEs', 'NOILC'}, {'$\beta_{rel} = 0$', '$\beta_{rel} = 0{,}5$', '$\beta_{rel} = 2$', '$\beta_{rel} = 5$'}};
+args.x_cell = {iter_vec, data_stochastic.t_vec, iter_vec, iter_vec(2:end)};
+args.y_cell = {{data_heuristic.SISO_MOLE.ILC_SISO.RMSE_log, data_stochastic.SISO_MOLE.ILC_SISO.RMSE_log, data_modelbased.ILC_Quadr.RMSE_log},
+    {data_stochastic.SISO_MOLE.ILC_SISO.r_vec, data_heuristic.SISO_MOLE.y_cell{end}, data_stochastic.SISO_MOLE.y_cell{end}, data_modelbased.ILC_Quadr.y_cell{end}},
+    {data_varbeta.SISO_MOLE_Cell{1, 1}.ILC_SISO.RMSE_log, data_stochastic.SISO_MOLE.ILC_SISO.RMSE_log, data_varbeta.SISO_MOLE_Cell{2, 1}.ILC_SISO.RMSE_log, data_varbeta.SISO_MOLE_Cell{3, 1}.ILC_SISO.RMSE_log},
+    {data_varbeta.SISO_MOLE_Cell{1, 1}.alpha_log, data_stochastic.SISO_MOLE.alpha_log, data_varbeta.SISO_MOLE_Cell{2, 1}.alpha_log, data_varbeta.SISO_MOLE_Cell{3, 1}.alpha_log}};
+args.x_label_cell = {'Iteration', '$t$ in $\mathrm{s}$', 'Iteration', 'Iteration'};
+args.y_label_cell = {'RMSE in $\mathrm{m}$', '$y_{L,10}$ in $\mathrm{m}$', 'RMSE in $\mathrm{m}$', '$\eta$'};
+args.title_cell = {'\textbf{(a)}', '\textbf{(b)}', '\textbf{(c)}', '\textbf{(d)}'};
+args.legend_cell = {{'MOLE', 'MOLEs', 'NOILC'}, {'Soll', 'MOLE', 'MOLEs', 'NOILC'}, {'$\beta_{rel} = 0$', '$\beta_{rel} = 0{,}5$', '$\beta_{rel} = 2$', '$\beta_{rel} = 5$'}, {'$\beta_{rel} = 0$', '$\beta_{rel} = 0{,}5$', '$\beta_{rel} = 2$', '$\beta_{rel} = 5$'},};
 
 args.filename = fullfile('05_Ergebnisse_Diskussion', 'Ergebnis_Osz_linear_vergleich.pdf');
 args.save_pdf = save_pdf;
 
 % Assign values (opts)
 opts = struct();
-opts.fig_height = 6.5;
+opts.fig_height = 10.5;
 opts.linewidth = 1.5;
-opts.y_scale = 'log';
-opts.y_lim = {[], []};
-opts.x_lim = {[], []};
-opts.marker = {'none', 'none'};
+opts.y_scale = {'log', 'linear', 'log', 'linear'};
+opts.y_lim = {[], [], [], []};
+opts.x_lim = {[], [], [], [0, 10]};
+opts.marker = {'none', 'none', 'none', '.'};
 
 % Create Plot
 plot = Plot_Manager(args);
-orientation = [1, 2];
+orientation = [2, 2];
 plot.tiled_plot(opts, orientation);
